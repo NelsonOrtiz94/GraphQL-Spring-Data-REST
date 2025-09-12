00025e3782724db3c8df1cb6d1476b68b614957a
NelsonOrtiz94 <ortnn990@gmail.com>
2025-09-12 15:06:13 -0500

    Primer commit

---
+++ src/main/java/com/cuenta/CuentaBancariaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cuenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentaBancariaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentaBancariaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cuenta/CuentaBancariaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentaBancariaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
